281860da02f9037da04a21222be2f4f7a22fcc4b
akcaau <gllazrv@gmail.com>
2022-12-09 20:41:25 +0300

    bfkdjb

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
-    
 ]
